8bfd654f5cb973a4e915b5fcb17c302bcdc092af
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-08 06:43:53 +0530

    installations

---
+++ installations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+userid=$(id -u)
+echo "userid is: $userid"
+
+if [ $userid -eq 0 ]
+then 
+echo "exit status is success"
+else
+echo "please run this script with the root privileges"
+fi 
+
+dnf install git -y 
