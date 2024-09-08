9670c8ec86a8250d3a8a4fedc0068eb235a57c84
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-08 07:54:55 +0530

    loops

--- day-12-practice.sh
+++ day-12-practice.sh
@@ -21,19 +21,27 @@ checking_root(){
     fi 
 }
 
-# validating(){
-#     if [ $1 -ne 0 ]
-#     then 
-#     echo "$2 is failed"
-#     exit 12 
-#     else 
-#     echo "$2 is succeded"
-#     fi 
-# }
+validating(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "$2 is failed"
+    exit 12 
+    else 
+    echo "$2 is succeded"
+    fi 
+}
 
 checking_root
 
 for package in $@
 do 
-echo $package 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then 
+        echo "$package is not installed.please install it now"
+        dnf install $package -y 
+        validating $? "Installing mysql is:"
+    else 
+       echo "Installing $package"
+    fi 
 done 
