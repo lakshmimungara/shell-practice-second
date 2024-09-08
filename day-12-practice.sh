f52d64d680585dac3ff1c8b7210d006d46d6663d
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-08 08:38:01 +0530

    loops

--- day-12-practice.sh
+++ day-12-practice.sh
@@ -12,7 +12,7 @@
 
 folder="/var/log/shell-script"
 mkdir -p $folder 
-script_name=$(echo "day-12-practice" | cut -d "." -f1)
+script_name=$(echo $0 | cut -d "." -f1)
 time=$(date +%Y-%m-%d-%H)
 log_file="$folder/$script_name-$time.log"
 
@@ -26,19 +26,19 @@ N="\e[30m"
 checking_root(){
     if [ $userid -eq 0 ]
     then 
-        echo -e "$G It is already following with the root priviledges $N"
+        echo -e "$G It is already following with the root priviledges $N" &>>$log_file
     else
-        echo -e "$R Please make sure it should be with the root priviledges $N"
+        echo -e "$R Please make sure it should be with the root priviledges $N" &>>$log_file
     fi 
 }
 
 validating(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$R $2 is failed $N"
+        echo -e "$R $2 is failed $N" &>>$log_file
         exit 12 
     else 
-        echo -e "$G $2 is succeded $N"
+        echo -e "$G $2 is succeded $N" &>>$log_file
     fi 
 }
 
@@ -59,10 +59,10 @@ do
     dnf list installed $package
     if [ $? -ne 0 ]
     then 
-        echo -e "$R $package is not installed.please install it now $N"
-        dnf install $package -y 
-        validating $? "Installing $package:"
+        echo -e "$R $package is not installed.please install it now $N" &>>$log_file
+        dnf install $package -y  &>>$log_file
+        validating $? "Installing $package:" 
     else 
-       echo -e "$G $package is already installed $N"
+       echo -e "$G $package is already installed $N" &>>$log_file
     fi 
 done 
