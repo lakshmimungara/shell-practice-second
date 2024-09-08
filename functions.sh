18839f658dcd294aa27e9b8f691416cdf1a438bd
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-08 07:12:34 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+userid=$(id -u)
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "$2 is success"
+    else
+        echo "$2 is failed, please check"
+    fi 
+}
+
+if [ $userid -ne 0 ]
+then 
+    echo " please run the script with root priviledges"
+    exit 127 
+else 
+    echo "it is already with the root priviledges"
+fi 
+
+dnf list installed mysql
+
+validate $? "listing the mysql"
