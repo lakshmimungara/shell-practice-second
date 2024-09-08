634195cbc649e8447faf25915087837dbcae8130
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-08 07:48:22 +0530

    loops

--- day-12-practice.sh
+++ day-12-practice.sh
@@ -8,3 +8,32 @@ echo $i
 done 
 
 
+# packages installation 
+
+userid=$(id -u)
+
+checking_root(){
+    if [ $userid -eq 0 ]
+    then 
+    echo "It is already following with the root priviledges"
+    else
+    echo "Please make sure it should be with with root priviledges"
+    fi 
+}
+
+# validating(){
+#     if [ $1 -ne 0 ]
+#     then 
+#     echo "$2 is failed"
+#     exit 12 
+#     else 
+#     echo "$2 is succeded"
+#     fi 
+# }
+
+checking_root
+
+for package in $@
+do 
+echo $package 
+done 
